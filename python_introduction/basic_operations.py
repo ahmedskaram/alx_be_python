f6900655382c7177ff4a14c8fa4be3f04ed25554
ahmedskaram <ahmedkaramak47@gmail.com>
2024-09-14 00:47:51 -0700

    Basic Arithmetic

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+
+addition = 10 + 5
+subtraction = 10 - 5
+multiplication = 10 * 5
+
+print('Addition of 10 and 5 is', addition)
+print('Subtraction of 10 and 5 is', subtraction)
+print('Multiplication of 10 and 5 is', multiplication)
+
