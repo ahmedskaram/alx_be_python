48d3457a682cbd9b45130a1f3df8a901b99e656b
ahmedskaram <ahmedkaramak47@gmail.com>
2024-09-29 12:19:58 -0700

    explore_datetime

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -2,12 +2,12 @@ from datetime import datetime
 
 def display_current_datetime():
     current_date = datetime.datetime.now()
-    print(f'Current date and time: {current_date}')
+    print(f'Current date and time: {current_date.strftime("%Y-%m-%d %H:%M:%S")}')
 display_current_datetime()
 
 def calculate_future_date():
     number_of_days = int(input('Ent number of days (as an integer): '))
     current_date = datetime.datetime.now()
     future_date  = current_date + datetime.timedelta(days=number_of_days)
-    print(f'Future date: {future_date}')
+    print(f'Future date: {future_date}.strftime("%Y-%m-%d")')
 calculate_future_date()
