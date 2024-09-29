696461455bb0dfcb929247d66621fbee6ab701cb
ahmedskaram <ahmedkaramak47@gmail.com>
2024-09-29 11:00:13 -0700

    shopping_list_manager

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -1,5 +1,11 @@
-
 shopping_lis = []
+
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
 
 while True:
     choice = input('Enter your choice: ')
