7a9624bfd226b4b165cd8f0e4ad38ce0e40e798d
ahmedskaram <ahmedkaramak47@gmail.com>
2024-09-29 09:46:11 -0700

    arithmetic_operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,13 +1,18 @@
 def perform_operation(num1, num2, operation):
+    
     match operation:
+        
         case '+':
-            return num1 + num2
-        case '_':
-            return num1 - num2
+            add = num1 + num2
+            return add
+        case '-':
+            subtract = num1 - num2
+            return subtract
         case '*':
-            return num1 * num2
+            multiply = num1 * num2
+            return multiply
         case '/':
             if num2 == 0:
-                return 'Cannot divide by zero'
+                return 'Cannot divide by Zero'
             else:
                 return num1 / num2
