1d78f5e292a84df252dd20330fcfed66744c8ea1
ahmedskaram <ahmedkaramak47@gmail.com>
2025-03-09 10:53:42 +0200

    Basic Arithmetic Exercise

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+number1 = 10
+number2 = 5
+
+addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+
+# Using f-strings for cleaner and more readable output
+print(f'Addition of 10 and 5 is: {addition}')
+print(f'Subtraction of 10 and 5 is: {subtraction}')
+print(f'Multiplication of 10 and 5 is: {multiplication}')
+
+# Another way to print without using f-strings (using commas)
+print('Addition of 10 and 5 is', addition)
+print('Subtraction of 10 and 5 is', subtraction)
+print('Multiplication of 10 and 5 is', multiplication)
